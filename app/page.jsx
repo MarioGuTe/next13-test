a00688a27849e024d9a61961c020223daab333d3
MarioGuTe <mariogutierreztello@gmail.com>
2023-04-16 01:35:37 -0400

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+// ésta es la página principal donde se renderiza inicialmente la
+// app
+// Las distintas vistas se escriben dentro de carpetas con el nombre
+// de la vista con una "page.jsx" dentro de la carpeta
+// El archivo layout.jsx envuelve la aplicación para por ejemplo renderizar
+// un mismo nav o footer etc a través de todas las vistas
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-lg py-2">Hello next 13 😁</h1>
+    </main>
+  );
+}
